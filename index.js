93a43bd9937bd405ab1b15e1edadf04fab223dfc
hariz <hariznyp@gmail.com>
2022-12-23 20:23:28 +0000

    migrations for weak entity

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ async function main() {
     app.get('/', (req,res)=>{
         res.send("It's alive!")
     })
+    app.get('/', (req,res)=>{
+        res.send("It's alive!")
+    })
 }
 
 main();
