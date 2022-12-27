6998a3949eb39b01e303a2dcfb3144726a4e7df9
hariz <hariznyp@gmail.com>
2022-12-27 08:03:09 +0000

    fix bug for updating form

--- routes/product.js
+++ routes/product.js
@@ -33,7 +33,6 @@ router.get('/', async (req,res)=>{
     })
 })
 
-
 router.get('/create', async (req,res)=>{
     const brands = await Brand.fetchAll().map((each)=>
     {
@@ -90,7 +89,6 @@ router.post('/create', async (req,res)=>
         }
     })
 })
-
 router.get('/:product_id/update', async (req, res) => {
     // retrieve the product
     const brands = await Brand.fetchAll().map((each)=>
@@ -111,7 +109,8 @@ router.get('/:product_id/update', async (req, res) => {
     const product = await Shoe.where({
         'id': productId
     }).fetch({
-        require: true
+        require: true,
+        withRelated:['materials']
     });
     console.log(productId)
     const productForm = createProductForm(brands,genders,materials);
@@ -123,27 +122,55 @@ router.get('/:product_id/update', async (req, res) => {
     productForm.fields.brand_id.value = product.get('brand_id');
     productForm.fields.gender_id.value = product.get('gender_id');
     
+    let selectedMaterials = await product.related('materials').pluck('id');
+    productForm.fields.materials.value = selectedMaterials;
+
     res.render('products/update', {
         'form': productForm.toHTML(bootstrapField),
         'product': product.toJSON()
     })
 })
-
 router.post('/:product_id/update', async (req, res) => {
-
     // fetch the product that we want to update
+    const brands = await Brand.fetchAll().map((each)=>
+    {
+        return[each.get('id'), each.get('brand')]
+    })
+    const genders = await Gender.fetchAll().map((each)=>
+    {
+        return[each.get('id'), each.get('gender')]
+
+    })
+    const materials = await Material.fetchAll().map((each)=>
+    {
+        return[each.get('id'), each.get('materials')]
+    })
+
+    const productId = req.params.product_id;
     const product = await Shoe.where({
-        'id': req.params.product_id
+        'id': productId
     }).fetch({
-        require: true
+        require: true,
+        withRelated:['materials']
     });
 
     // process the form
-    const productForm = createProductForm();
+    const productForm = createProductForm(brands,genders,materials);
     productForm.handle(req, {
         'success': async (form) => {
-            product.set(form.data);
+            
+            let{materials,...productData} = form.data;
+            product.set(productData);
             product.save();
+            //update tags
+            let materialIds = materials.split(',')
+            let existingMaterialIds = await product.related('materials').pluck('id');
+
+            //Remove all the materials that aren't selected anyomre
+            let toRemove = existingMaterialIds.filter(id => materialIds.includes(id) === false);
+            await product.materials().detach(toRemove);
+            await product.materials().attach(materialIds);
+
             res.redirect('/products');
         },
         'error': async (form) => {
